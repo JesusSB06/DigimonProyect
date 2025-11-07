ccc3b9621e11739cb24d5c517065c3f3bdc4ae3c
a24josedf <a24josedf@iesantonlosada.gal>
2025-11-07 11:23:34 +0100

    Commit added image, level , type classes

---
+++ src/main/java/model/Image.java
@@ -0,0 +1,27 @@
+package model;
+
+/**
+ *
+ * @author dam2_alu04@inf.ald
+ */
+public class Image {
+
+    private String href;
+    private boolean transparent;
+
+    public String getHref() {
+        return href;
+    }
+
+    public void setHref(String href) {
+        this.href = href;
+    }
+
+    public boolean isTransparent() {
+        return transparent;
+    }
+
+    public void setTransparent(boolean transparent) {
+        this.transparent = transparent;
+    }
+}
---
+++ src/main/java/model/Type.java
@@ -0,0 +1,27 @@
+package model;
+
+/**
+ *
+ * @author dam2_alu04@inf.ald
+ */
+public class Type {
+
+    private int id;
+    private String type;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
