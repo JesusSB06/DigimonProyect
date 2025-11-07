5e6207ba9b5d815da5497973bf4db461b0cc11b0
JesusSB06 <a24jesussb@iesantonlosada.gal>
2025-11-07 10:21:59 +0100

    Segundo commit

---
+++ src/main/java/com/mycompany/digimonproyect/main/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.digimonproyect.main;
+
+/**
+ *
+ * @author dam2_alu19@inf.ald
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
