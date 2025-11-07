25baa57c05b5e6a112a7148dfa3b4a4a0378b367
JesusSB06 <a24jesussb@iesantonlosada.gal>
2025-11-07 11:02:11 +0100

    Tercer commit: Creación de primeros POJO

---
+++ src/main/java/com/mycompany/digimonproyect/DigimonProyect.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.digimonproyect;
+
+import com.mycompany.digimonproyect.service.ApiConnection;
+
+/**
+ *
+ * @author dam2_alu19@inf.ald
+ */
+public class DigimonProyect {
+
+    public static void main(String[] args) {
+       ApiConnection.askApi("Agumon");
+    }
+}
---
+++ src/main/java/model/Description.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author dam2_alu19@inf.ald
+ */
+public class Description {
+    private String origin;
+    private String language;
+    private String description;
+
+    public String getOrigin() {
+        return origin;
+    }
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+    public String getLanguage() {
+        return language;
+    }
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+}
