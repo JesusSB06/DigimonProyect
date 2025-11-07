3208693746b28ece8c0572c47a2422f3ecb96818
Anonymous180937642012 <a24adanol@iesantonlosada.gal>
2025-11-07 16:08:13 +0100

    Field & Attribute
    Planteo inicial de los POJO de Field y Attribute

---
+++ src/main/java/model/Field.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author ADAN
+ */
+public class Field {
+    private int id;
+    private String field;
+    private Image image;
+
+    public Field() {
+    }
+
+    public Field(int id, String field, Image image) {
+        this.id = id;
+        this.field = field;
+        this.image = image;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getField() {
+        return field;
+    }
+
+    public void setField(String field) {
+        this.field = field;
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    public void setImage(Image image) {
+        this.image = image;
+    }
+    
+}
