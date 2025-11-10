dfa69ca804f4a48de2f515ba23a43382daf3f64a
Jesus Santos Baquero <a24jesussb@iesantonlosada.gal>
2025-11-10 18:23:50 +0100

    Modificacion para realizar el full request

--- src/main/java/com/mycompany/digimonproyect/service/ApiConnection.java
+++ src/main/java/com/mycompany/digimonproyect/service/ApiConnection.java
@@ -19,42 +19,4 @@ import java.util.logging.Logger;
  */
 public class ApiConnection {
 
-    private static final String DIGIMON = "https://digi-api.com/api/v1/digimon/";
-
-    public static void askApi(String nameDigimon) {
-        try {
-            URL urlJson = new URL(DIGIMON + nameDigimon);
-            HttpURLConnection connection = (HttpURLConnection) urlJson.openConnection();
-            connection.setRequestMethod("GET");
-
-            // Conecta con el servidor
-            int responseCode = connection.getResponseCode();
-
-            // Verifica si la conexión fue exitosa
-            if (responseCode == HttpURLConnection.HTTP_OK) {
-                System.out.println("✅ Conexión exitosa. Código: " + responseCode);
-
-                // Leer la respuesta (opcional)
-                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                String line;
-                StringBuilder response = new StringBuilder();
-                while ((line = reader.readLine()) != null) {
-                    response.append(line);
-                }
-                reader.close();
-
-                // Mostrar el JSON recibido
-                System.out.println("Respuesta: " + response.toString());
-            } else {
-                System.out.println("❌ Error al conectar. Código: " + responseCode);
-            }
-
-            // Cierra la conexión
-            connection.disconnect();
-        } catch (MalformedURLException ex) {
-            Logger.getLogger(ApiConnection.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException ex) {
-            Logger.getLogger(ApiConnection.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
 }
