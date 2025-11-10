0d4552f3bac8446c36020540f0861bdda47c49c7
dam2_alu19@inf.ald <a24jesussb@iesantonlosada.gal>
2025-11-10 12:44:04 +0100

    Todos los archivos bien estructurados y preparados para su uso

--- src/main/java/com/mycompany/digimonproyect/model/Field.java
+++ src/main/java/com/mycompany/digimonproyect/model/Field.java
@@ -2,7 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package model;
+package com.mycompany.digimonproyect.model;
+
+import com.mycompany.digimonproyect.model.Image;
 
 /**
  *
