b6e3aa1685a6f6599ca2f599a2ef4f2bce8fa30d
Anonymous180937642012 <a24adanol@iesantonlosada.gal>
2025-11-18 20:45:45 +0100

    Imagenes y Apodos

--- src/main/java/com/mycompany/digimonproyect/controller/PersonalListController.java
+++ src/main/java/com/mycompany/digimonproyect/controller/PersonalListController.java
@@ -3,9 +3,20 @@ package com.mycompany.digimonproyect.controller;
 import com.mycompany.digimonproyect.model.digimon.Digimon;
 import com.mycompany.digimonproyect.model.users.Users;
 import com.mycompany.digimonproyect.view.PersonalListJDialog;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.Vector;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
 
 public class PersonalListController {
@@ -30,7 +41,21 @@ public class PersonalListController {
         view.clearTable();
         for (Digimon d: model.getCurrentUser().getDigimon()) {
             Vector row = new Vector();
-            row.add("Imagen");
+            try {
+                URL url = new URI(d.getImages().get(0).getHref()).toURL();
+                BufferedImage img = ImageIO.read(url);
+                int rowHeight = 80;
+                int newWidth = img.getWidth() * rowHeight / img.getHeight();
+                Image scaled = img.getScaledInstance(newWidth, rowHeight, Image.SCALE_SMOOTH);
+                ImageIcon icon = new ImageIcon(scaled);
+                row.add(icon);
+            } catch (URISyntaxException ex) {
+                Logger.getLogger(PersonalListController.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (MalformedURLException ex) {
+                Logger.getLogger(PersonalListController.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (IOException ex) {
+                Logger.getLogger(PersonalListController.class.getName()).log(Level.SEVERE, null, ex);
+            }
             row.add(d.getNickname());
             row.add(d.getName());
             row.add(true);
@@ -43,7 +68,7 @@ public class PersonalListController {
             @Override
             public void actionPerformed(ActionEvent ae) {
                 model.getCurrentUser().delDigimon(view.getSelectionInt());
-                 updateTable();
+                updateTable();
             }
         };
         return al;
@@ -52,7 +77,13 @@ public class PersonalListController {
         ActionListener al = new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent ae) {
-                
+                if (view.getSelectionInt()!=-1) {
+                    String nickname = JOptionPane.showInputDialog(view, "Write the nickname for your digimon:");
+                    model.getCurrentUser().changeDigimonNickname(view.getSelectionInt(), nickname);
+                    updateTable();
+                } else {
+                    JOptionPane.showMessageDialog(view, "Please, select a digimon in the list to change the nickname to");
+                }
             }
         };
         return al;
